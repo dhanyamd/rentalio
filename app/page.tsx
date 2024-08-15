4adf3f7eba166a9f6594a160ef3a220efd25c686
Dhanya MD <dhanya13md@gmail.com>
2024-08-15 23:50:00 +0530

    last

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
 import { Suspense } from "react";
 import getCurrentUser from "./actions/getCurrentUser";
-import getListings from "./actions/getListings";
+import getListings, { IListingParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
-import { ISearchListingParams } from "./types";
+import { ISearchListingParams, safeListing } from "./types";
 
 
 export const dynamic = 'force-dynamic';
 
 interface HomeProps {
-  searchParams: ISearchListingParams;
+  searchParams: IListingParams;
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
@@ -39,13 +39,9 @@ const Home = async ({ searchParams }: HomeProps) => {
       <Container>
         <Suspense>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => (
-            <ListingCard
-              currentUser={currentUser}
-              data={listing}
-              key={listing.id}
-            />
-          ))}
+        {listings.map((listing: safeListing) => {
+          return <ListingCard currentUser={currentUser} key={listing.id} data={listing} />;
+        })}
         </div>
         </Suspense>
       </Container>
